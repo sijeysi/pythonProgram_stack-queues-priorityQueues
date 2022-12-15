2da4e3264bbcef37533cdb44fc4fac8f1121a7a7
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 10:52:01 +0800

    Program is running

--- queue_FIFO.py
+++ queue_FIFO.py
@@ -6,9 +6,6 @@ fifo.enqueue("1st")
 fifo.enqueue("2nd")
 fifo.enqueue("3rd")
 
-fifo.dequeque()
-'1st'
-fifo.dequeque()
-'2nd'
-fifo.dequeque()
-'3rd'
+print(fifo.dequeque())
+print(fifo.dequeque())
+print(fifo.dequeque())
