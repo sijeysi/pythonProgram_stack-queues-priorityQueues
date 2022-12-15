e3c8bc60495c762090158fd8a5897ee89660235b
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 00:16:09 +0800

    Change somtething

--- queue_FIFO1.py
+++ queue_FIFO1.py
@@ -2,17 +2,13 @@ from queues import Queue
 
 fifo = Queue("1st", "2nd", "3rd")
 
-print(len(fifo))
-# Expected Output
-# 3
+print(len(fifo))    # Output: 3
 
 for element in fifo:
     print(element)
-# Expected Output
+# Output:
 # 1st 
 # 2nd
 # 3rd
 
-print(len(fifo))
-# Expected Output
-# 0
+print(len(fifo))    # Output: 0
