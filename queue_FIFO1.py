760381dbc0450add15c36f3041bfa616a0ba7d4f
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 13:13:52 +0800

    Completed queue_FIFO1.py

--- queue_FIFO1.py
+++ queue_FIFO1.py
@@ -1,9 +1,18 @@
 from queues import Queue
 
 fifo = Queue("1st", "2nd", "3rd")
+
 print(len(fifo))
+# Expected Output
+# 3
 
 for element in fifo:
     print(element)
+# Expected Output
+# 1st 
+# 2nd
+# 3rd
 
 print(len(fifo))
+# Expected Output
+# 0
