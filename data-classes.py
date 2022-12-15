8cee6c485614587189fd289cb150277dfd664ed3
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 22:48:23 +0800

    Program data-classes is working properly so fa

--- data-classes.py
+++ data-classes.py
@@ -3,3 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class Message:
     event: str 
+
+wipers = Message("Winshield wipers turned on")
+hazard_lights = Message("Hazard loghts turned on")
+
+wipers < hazard_lights
