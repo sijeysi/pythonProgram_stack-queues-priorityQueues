9a5229eef0addabc44b9f3401b082960ac709018
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 18:36:27 +0800

    Completed stack_LIFO1.py

--- stack_LIFO1.py
+++ stack_LIFO1.py
@@ -4,6 +4,8 @@ lifo.append("1st")
 lifo.append("2nd")
 lifo.append("3rd")
 
-print(lifo.pop())
-print(lifo.pop())
-print(lifo.pop())
+print(lifo.pop())   # Output: 3rd
+print(lifo.pop())   # Output: 2nd
+print(lifo.pop())   # Output: 1st
+
+print("done")
