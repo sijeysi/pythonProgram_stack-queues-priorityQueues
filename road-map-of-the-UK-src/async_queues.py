19053d2b095c51a516841ef20a5092a588d90d8b
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 02:08:43 +0800

    Typed out all inside try

--- road-map-of-the-UK-src/async_queues.py
+++ road-map-of-the-UK-src/async_queues.py
@@ -19,3 +19,26 @@ class Job(NamedTuple):
         if isinstance(other, Job):
             return len(self.url) < len(other.url)
 
+
+async def main(args):
+    session = aiohttp.ClientSession()
+    try:
+        links = Counter()
+        queue = asyncio.Queue()
+        # queue = asyncio.LifoQueue()
+        # queue = asyncio.PriorityQueue()
+        tasks = [
+            asyncio.create_task(
+                worker(
+                    f"Worker-{i + 1}",
+                    session,
+                    queue,
+                    links,
+                    args.max_depth,
+                )
+            )
+            for i in range(args.num_workers)
+        ]
+
+        await queue.put(Job(args.url))
+        await queue.join()
