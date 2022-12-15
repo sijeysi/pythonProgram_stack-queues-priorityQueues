4f4b42e6d89b632536c542fdac198f54f71ff79f
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 23:00:11 +0800

    The added display the same output in website

--- data-classes.py
+++ data-classes.py
@@ -10,9 +10,12 @@ hazard_lights = Message("Hazard loghts turned on")
 wipers < hazard_lights  # Output: TypeError: '<' not supported between instances of 'Message' and 'Message'
 
 ...
+
 messages = PriorityQueue()
 
 messages.enqueue_with_priority(CRITICAL, wipers)
 messages.enqueue_with_priority(IMPORTANT, hazard_lights)
 
 messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
+
+# Output: TypeError: '<' not supported between instances of 'Message' and 'Message'
