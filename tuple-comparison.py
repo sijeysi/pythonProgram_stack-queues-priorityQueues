c085e2a1594538d585a17c6a701d45d3a9d7f2e9
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 19:03:49 +0800

    Completed tuple-comparison.py

--- tuple-comparison.py
+++ tuple-comparison.py
@@ -2,5 +2,5 @@ person1 = ("John", "Brown", 42)
 person2 = ("JOhn", "Doe", 42)
 person3 = ("John", "Doe", 24)
 
-print(person1 < person2)
-print(person2 < person3)
+print(person1 < person2)    # Output: True
+print(person2 < person3)    # Output: False
