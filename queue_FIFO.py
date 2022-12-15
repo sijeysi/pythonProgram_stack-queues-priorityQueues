abb4245b1ecd9ed2c2f061b0eaec2c4d50ea9ed9
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 00:15:55 +0800

    Change something

--- queue_FIFO.py
+++ queue_FIFO.py
@@ -6,11 +6,6 @@ fifo.enqueue("1st")
 fifo.enqueue("2nd")
 fifo.enqueue("3rd")
 
-print(fifo.dequeque())
-print(fifo.dequeque())
-print(fifo.dequeque())
-
-# Expected Output
-# 1st
-# 2nd 
-# 3rd
+print(fifo.dequeue())   # Output: 1st
+print(fifo.dequeue())   # Output: 2nd
+print(fifo.dequeue())   # Output: 3rd
