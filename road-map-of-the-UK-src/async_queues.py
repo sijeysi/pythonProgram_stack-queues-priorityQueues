cb4edf4c7554900279c6731ca3505ebace0d1b80
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 02:42:05 +0800

    Typed out def display(links):

--- road-map-of-the-UK-src/async_queues.py
+++ road-map-of-the-UK-src/async_queues.py
@@ -72,3 +72,8 @@ def parse_args():
     parser.add_argument("-d", "--max-depth", type=int, default=2)
     parser.add_argument("-w", "--num-workers", type=int, default=3)
     return parser.parse_args()
+
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")
+        
