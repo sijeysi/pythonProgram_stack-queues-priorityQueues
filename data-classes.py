b52ad743e379eba770837d5910ca859f05564d90
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 22:57:45 +0800

    I moved the content of priority-queue.py inside sata-classes.py

--- data-classes.py
+++ data-classes.py
@@ -8,3 +8,11 @@ wipers = Message("Winshield wipers turned on")
 hazard_lights = Message("Hazard loghts turned on")
 
 wipers < hazard_lights  # Output: TypeError: '<' not supported between instances of 'Message' and 'Message'
+
+...
+messages = PriorityQueue()
+
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
+
+messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
