9b7a1064ff87203068919e1603a9c3eac17fedeb
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 18:54:05 +0800

    Completed heap.py

--- heap.py
+++ heap.py
@@ -9,5 +9,5 @@ heappush(fruits, "banana")
 print(fruits)   # Output: ['apple', 'orange', 'banana']
 
 from heapq import heappop
-heappop(fruits)
-print(fruits)
+heappop(fruits) # Popping/removing the first element
+print(fruits)   # Output: ['banana', 'orange']
