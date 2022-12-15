70fa193437bc64d463dd4e2d6f205d22ac54da4b
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 04:26:39 +0800

    Another def

--- representation-of-the-cities-and-roads/cities-and-roads-src.py
+++ representation-of-the-cities-and-roads/cities-and-roads-src.py
@@ -2,3 +2,5 @@ import networkx as nx
 
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 print(graph.nodes["london"])
+
+
--- representation-of-the-cities-and-roads/graph.py
+++ representation-of-the-cities-and-roads/graph.py
@@ -1,4 +1,5 @@
 from typing import NamedTuple
+import networkx as nx
 
 class City(NamedTuple):
     name: str
@@ -16,3 +17,15 @@ class City(NamedTuple):
             latitude = float(attrs["latitude"]),
             longitude = float(attrs["longitude"]),
         )
+    
+    def load_graph(filename, node_factory):
+        graph = nx.nx_agraph.read_dot(filename)
+        nodes = {
+            name : node_factory(attributes)
+            for name, attributes in graph.nodes(data=True)
+        }
+        return nodes, nx.Graph(
+            (nodes[name1], nodes[name2], weights)
+            for name1, name2, weights in graph.edges(data=True)
+        )
+        
