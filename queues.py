fa3531ca5f705e0977257baa443d9c1566251771
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 12:28:49 +0800

    Initialiazed the import dequeue

--- queues.py
+++ queues.py
@@ -1,3 +1,4 @@
+# A class that will delegate the enqueue and dequeue operations to dequeue.append() and dequeue.popleft()
 from collections import deque
 
 class Queue:
@@ -9,3 +10,6 @@ class Queue:
 
     def dequeque(self):
         return self.__elements.popleft()
+
+# An iterable class and will able to report its length and accept initial elements
+from collections import deque
