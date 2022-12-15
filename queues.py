3cb6dfbd35bffa6a81e75f31f2d10ca0e027d631
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-15 19:24:12 +0800

    Made some edits

--- queues.py
+++ queues.py
@@ -1,29 +1,9 @@
-# A class that will delegate the enqueue and dequeue operations to dequeue.append() and dequeue.popleft()
 from collections import deque
 from heapq import heappop, heappush
-from itertools import count
 
-class IterableMixin:
-    def __len__(self):
-        return len(self.elements)
-
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
-            
-class Queue:
-    def __init__(self):
-        self.__elements = deque()
-    
-    def enqueue(self, element):
-        self.__elements.append(element)
-
-    def dequeue(self):
-        return self.__elements.popleft()
-
-
-# Making the class iterable and able to report its length and accept initial elements
-class Queue(IterableMixin):   
+# A class that will delegate the enqueue and dequeue operations to dequeue.append() and dequeue.popleft()
+class Queue():
+    # Making the class iterable and able to report its length and accept initial elements
     def __init__(self, *elements):
         self.__elements = deque(elements)
 
@@ -43,7 +23,7 @@ class Queue(IterableMixin):
 # A class using inheritance and override the .dequeue() method to remove elements from the top of the stack
 class Stack(Queue):
     def dequeue(self):
-        return self.__elements.pop()
+        return self._elements.pop()
 
 # class PriorityQueue
 class PriorityQueue:
