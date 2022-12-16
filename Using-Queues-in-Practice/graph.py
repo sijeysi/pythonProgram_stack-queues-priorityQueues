594a1283a73ca596630cc503dc99ff37eb47bf13
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 17:54:34 +0800

    Modify the folder

---
+++ Using-Queues-in-Practice/graph.py
@@ -0,0 +1,49 @@
+
+from typing import NamedTuple
+import networkx as nx
+from queues import Queue
+
+# Object Representation of the Cities and Roads
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name = attrs["xlabel"],
+            country = attrs["country"],
+            year = int(attrs["year"]) or None,
+            latitude = float(attrs["latitude"]),
+            longitude = float(attrs["longitude"]),
+        )
+    
+def load_graph(filename, node_factory):
+    graph = nx.nx_agraph.read_dot(filename)
+    nodes = {
+        name : node_factory(attributes)
+        for name, attributes in graph.nodes(data=True)
+    }
+    return nodes, nx.Graph(
+        (nodes[name1], nodes[name2], weights)
+        for name1, name2, weights in graph.edges(data=True)
+    )
+
+# Breadth-First Search Using a FIFO Queue
+def breadth_first_traverse(graph, source):
+    queue = Queue(source)
+    visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
+        for neighbor in graph.neighbors(node):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+
+def breadth_first_search(graph, source, predicate):
+    for node in breadth_first_traverse(graph, source):
+        if predicate(node):
+            return node
