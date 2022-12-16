84ce5c40fdcac6f605959851a1a16ed98cfef8f8
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 18:16:07 +0800

    Shortest Path first 12 lines

---
+++ Using-Queues-in-Practice/shortest-path.py
@@ -0,0 +1,12 @@
+# Shortest Path Using Breadth-First Traversal
+import networkx as nx
+from graph import City, load_graph
+
+print("\n8th testing: Using networkx to reveal all the shortest paths between two cities")
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", " → ".join(city.name for city in path))
