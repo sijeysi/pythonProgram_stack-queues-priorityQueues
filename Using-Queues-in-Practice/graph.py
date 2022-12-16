7435a0e554c1d9f12a467ea93d60293ba7c5921f
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 19:33:14 +0800

    Last in this folder completed in graph.py

--- Using-Queues-in-Practice/graph.py
+++ Using-Queues-in-Practice/graph.py
@@ -3,6 +3,8 @@ from typing import NamedTuple
 import networkx as nx
 from queues import Queue
 from collections import deque
+from math import inf as infinity
+from queues import MutableMinHeap, Queue, Stack
 
 # Object Representation of the Cities and Roads
 class City(NamedTuple):
@@ -131,3 +133,24 @@ def search(traverse, graph, source, predicate, order_by=None):
             return node
 
 
+# Dijkstra’s Algorithm Using a Priority Queue
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
+    while unvisited:
+        visited.add(node := unvisited.dequeue())
+        for neighbor, weights in graph[node].items():
+            if neighbor not in visited:
+                weight = weight_factory(weights)
+                new_distance = unvisited[node] + weight
+                if new_distance < unvisited[neighbor]:
+                    unvisited[neighbor] = new_distance
+                    previous[neighbor] = node
+
+    return retrace(previous, source, destination)
