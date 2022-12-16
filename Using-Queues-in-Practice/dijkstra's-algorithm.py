0ea7edf9d613153fb98063f2a3f5c0c1606fb99c
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 19:51:21 +0800

    extracted road map

--- Using-Queues-in-Practice/dijkstra's-algorithm.py
+++ Using-Queues-in-Practice/dijkstra's-algorithm.py
@@ -1,1 +1,14 @@
 # Dijkstra’s Algorithm Using a Priority Queue
+import networkx as nx
+from graph import City, load_graph, dijkstra_shortest_path
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city1 = nodes["london"]
+city2 = nodes["edinburgh"]
+
+def distance(weights):
+    return float(weights["distance"])
+
+for city in dijkstra_shortest_path(graph, city1, city2, distance):
+    print(city.name)
+
