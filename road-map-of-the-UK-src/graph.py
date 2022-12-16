07d655fdae2f9043a7d4720efd147b358ebae999
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 17:31:21 +0800

    Edits

--- road-map-of-the-UK-src/graph.py
+++ road-map-of-the-UK-src/graph.py
@@ -1,6 +1,7 @@
 
 from typing import NamedTuple
 import networkx as nx
+from queues import Queue
 
 class City(NamedTuple):
     name: str
@@ -29,3 +30,19 @@ def load_graph(filename, node_factory):
         (nodes[name1], nodes[name2], weights)
         for name1, name2, weights in graph.edges(data=True)
     )
+
+# Breadth-First Search Using a FIFO Queue
+def breadth_first_traverse(graph, source):
+    queue = Queue(source)
+    visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
+        for neighbor in graph.neighbors(node):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+
+def breadth_first_search(graph, source, predicate):
+    for node in breadth_first_traverse(graph, source):
+        if predicate(node):
+            return node
