93a521c8c3d796ef1439b73f59e18167489399d1
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 19:58:53 +0800

    Commit

--- Using-Queues-in-Practice/queues.py
+++ Using-Queues-in-Practice/queues.py
@@ -1,6 +1,8 @@
 from collections import deque
-from heapq import heappop, heappush
+from dataclasses import dataclass
+from heapq import heapify, heappop, heappush
 from itertools import count
+from typing import Any
 
 class IterableMixin:
     def __len__(self):
@@ -13,13 +15,6 @@ class IterableMixin:
 class Queue(IterableMixin):
     def __init__(self, *elements):
         self.__elements = deque(elements)
-
-    def __len__(self):
-        return len(self.__elements)
-
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
     
     def enqueue(self, element):
         self.__elements.append(element)
@@ -43,3 +38,33 @@ class PriorityQueue(IterableMixin):
 
     def dequeue(self):
         return heappop(self._elements)[-1]
+
+
+@dataclass(order=True)
+class Element:
+    priority: float
+    count: int
+    value: Any
+
+
+class MutableMinHeap(IterableMixin):
+    def __init__(self):
+        super().__init__()
+        self._elements_by_value = {}
+        self._elements = []
+        self._counter = count()
+
+    def __setitem__(self, unique_value, priority):
+        if unique_value in self._elements_by_value:
+            self._elements_by_value[unique_value].priority = priority
+            heapify(self._elements)
+        else:
+            element = Element(priority, next(self._counter), unique_value)
+            self._elements_by_value[unique_value] = element
+            heappush(self._elements, element)
+
+    def __getitem__(self, unique_value):
+        return self._elements_by_value[unique_value].priority
+
+    def dequeue(self):
+        return heappop(self._elements).value
