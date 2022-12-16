ff82bd6fee5f16babf0d7427ae5cec55dcc83fda
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 18:39:16 +0800

    Completed depth-first-search.py

--- Using-Queues-in-Practice/depth-first-search.py
+++ Using-Queues-in-Practice/depth-first-search.py
@@ -1,1 +1,16 @@
 # Depth-First Search Using a LIFO Queue
+import networkx as nx
+from graph import City, load_graph
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
+
+print(" ")
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.dfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
