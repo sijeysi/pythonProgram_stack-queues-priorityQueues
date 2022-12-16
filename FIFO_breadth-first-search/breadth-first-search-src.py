05e18b41cfd571074cd5dbf79750d4b95a67610f
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-12-16 17:15:07 +0800

    Completed the added in breadth-first-search-src.py

--- FIFO_breadth-first-search/breadth-first-search-src.py
+++ FIFO_breadth-first-search/breadth-first-search-src.py
@@ -12,3 +12,17 @@ for node in nx.bfs_tree(graph, nodes["edinburgh"]):
         break
 else:
     print("Not found")
+
+
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
+
+for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
